54deabc266c3ce72037219aedee1ccec4e5318c6
devcecode <devcecode@gmail.com>
2022-09-21 00:59:40 -0500

    v18

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from './components/header/Header'
 import Home from './pages/home/Home'
